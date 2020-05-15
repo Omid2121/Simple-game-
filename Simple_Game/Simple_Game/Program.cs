b949025cd4f04236fc7fe04e559a1aed7b6e0190
Omid2121 <Omid_Ahangi@github.com>
2020-05-15 12:03:16 -0700

    game score

--- Simple_Game/Simple_Game/Program.cs
+++ Simple_Game/Simple_Game/Program.cs
@@ -10,25 +10,59 @@ namespace Simple_Game
     {
         static int playerPosX = 1, playerPosY = 1;
         static int enemyPosX, enemyPosY;
+        static int itemPosX, itemPosY;
+        static int score, highScore;
         static int length = 10, height = 10;
-        static char player = '*', space = '-', enemy ='@';
+        static char player = '*', space = '-', enemy ='@', item = '$';
         static Random random = new Random();
-
 
 
         static void Main(string[] args)
         {
+            bool playing = true;
+
+            while (playing) 
+            {
+                Play();
+
+                if (score > highScore)
+                    highScore = score;
+
+                Console.WriteLine("\n Game Over! \n Final Score: " + highScore);                                             
+                                         
+
+                Console.WriteLine("Do you want to play again? (Y/N): ");
+                ConsoleKeyInfo keyPressed = Console.ReadKey();                                 
+
+                if (keyPressed.Key == ConsoleKey.N)               
+                    playing = false;
+                
+            }           
+               
+        }
+
+        static void Play()
+        {
+            playerPosX = 1;
+            playerPosY = 1;
             enemyPosX = length;
             enemyPosY = height;
-
+            score = 0;
+            CreatItem();
 
             while (true)
-            {             
+            {
                 Draw();
 
                 if (PlayerCollideWithEnemy())
                 {
                     break;
+                }
+                if (PlayerCollideWithItem())
+                {
+                    ++score;
+                    CreatItem();
+                    Draw();
                 }
 
                 ConsoleKeyInfo keyPressed = Console.ReadKey();
@@ -43,17 +77,17 @@ namespace Simple_Game
                 }
 
                 MoveEnemy();
-               
+
             }
-
-            Console.WriteLine("Game Over!");
-            Console.Read();
-
 
         }
 
         static void MoveEnemy()
         {
+            if (random.Next(0,11) < 2) 
+                return;
+            
+
             if ((random.Next(0, 11) >= 5 && playerPosX != enemyPosX) || playerPosY == enemyPosY)
             {
                 if (playerPosX > enemyPosX) --enemyPosX;
@@ -75,35 +109,56 @@ namespace Simple_Game
             
         }
 
+        static bool PlayerCollideWithItem()
+        {
+            if (playerPosX == itemPosX && playerPosY == itemPosY) return true;
+
+            return false;
+        }
+
+        static void CreatItem()
+        {
+            int x = random.Next(1, length), y = playerPosY;
+
+            while (y > playerPosY - 2 && y < playerPosY + 2)
+            {
+                y = random.Next(1, height);
+            }
+
+            itemPosX = x;
+            itemPosY = y;
+        }
+
         static void Draw ()
         {
             Console.Clear();
 
+            Console.WriteLine("Score: " + score + "\n");
             Console.WriteLine("(" + playerPosX + ", " + playerPosY + ")\n");
             Console.WriteLine("(" + enemyPosX + ", " + enemyPosY + ")\n");
 
-
-            for (int y = 1; y <= length; ++y)
+            for (int y = 1; y <= height; ++y)
             {
                 for (int x = 1; x <= length; ++x)
                 {
-                    if (x == playerPosX && y == playerPosY)
-                    {
-                        Console.WriteLine(player);
-                    }
-                    else if (x == enemyPosX && y == enemyPosY)
-
+                    if (x == enemyPosX && y == enemyPosY)
                         Console.WriteLine(enemy);
+
+                    else if (x == playerPosX && y == playerPosY)                   
+                        Console.WriteLine(player);
+                    
+
+                    else if (x == itemPosX && y == itemPosY)
+                        Console.WriteLine(item);
 
                     else
                         Console.WriteLine(space);
                 }
+
                 Console.WriteLine();
             }
 
         }
-
-
 
     }
 }
